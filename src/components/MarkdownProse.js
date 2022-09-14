2cd8141389ee0cc3ec33715712d13a241918c02b
Mark Philpot <mark.philpot@gmail.com>
2022-09-14 09:27:07 -0700

    Customizing inline code styling for prose

--- src/components/MarkdownProse.js
+++ src/components/MarkdownProse.js
@@ -5,7 +5,7 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 const MarkdownProse = (props) => {
   const { markdown } = props;
   return (
-    <article className="prose prose-xl max-w-none dark:prose-invert">
+    <article className="prose prose-xl max-w-none prose-code:before:content-none prose-code:after:content-none dark:prose-invert">
       <MDXRenderer>{markdown}</MDXRenderer>
     </article>
   );
