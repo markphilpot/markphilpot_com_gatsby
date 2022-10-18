dfedcf053bed79f6722707572eb2adba51aa9363
Mark Philpot <mark.philpot@gmail.com>
2022-10-17 19:41:43 -0700

    Tweak dark backgrounds again

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,10 +1,12 @@
-export default {
+const colors = {
   backgrounds: {
-    default: `bg-gray-100 dark:bg-slate-900`,
-    micro: `bg-slate-200 dark:bg-[#2E3250]`,
+    default: `bg-gray-100 dark:bg-[#11001c]`,
+    micro: `bg-slate-200 dark:bg-[#272640]`,
   },
   text: {
     default: `text-black dark:text-stone-300`,
     linkHover: `hover:text-fuchsia-700 hover:dark:text-fuchsia-400`,
   },
 };
+
+export default colors;
